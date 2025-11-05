1238466a2881cc3f70927e21447fc534bb72afd7
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-11-05 10:38:03 +0700

    config prettier, formatting code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,34 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  // {
+  //   rules: {
+  //     semi: "warn",
+  //   }
+  // },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': ['warn', {}, { usePrettierrc: true }],
+      // hoặc ["error", {}, { usePrettierrc: true }] nếu bạn muốn coi đó là lỗi
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
