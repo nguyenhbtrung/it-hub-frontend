ef6502a7a5ec2f815663f3ae8b1870ad272205b0
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-07-05 08:32:05 +0700

    fix(CourseDetailPage): comment out unused button for instructor profile link

--- src/components/user/course/instructorCard/index.tsx
+++ src/components/user/course/instructorCard/index.tsx
@@ -99,7 +99,7 @@ export default async function InstructorCard({ courseId }: InstructorCardProps) 
         </Box>
       </Stack>
 
-      <Box sx={{ textAlign: 'center', mt: 3 }}>
+      {/* <Box sx={{ textAlign: 'center', mt: 3 }}>
         <Button
           LinkComponent={Link}
           href={`/users/${instructor?.id}`}
@@ -109,7 +109,7 @@ export default async function InstructorCard({ courseId }: InstructorCardProps) 
         >
           Xem toàn bộ hồ sơ giảng viên
         </Button>
-      </Box>
+      </Box> */}
     </Section>
   );
 }
